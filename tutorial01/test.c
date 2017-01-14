7b457ca6598881c7d445bfd9a16ed6fed1e09f8f
Neuromancer <chenyifan_1997@hotmail.com>
2017-01-14 13:30:23 +0000

    fix bugs of tests

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -22,21 +22,21 @@ static int test_pass = 0;
 
 static void test_parse_null() {
     lept_value v;
-    v.type = LEPT_NULL;
+    v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_true() {
     lept_value v;
-    v.type = LEPT_TRUE;
+    v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
     EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
 }
 
 static void test_parse_false() {
     lept_value v;
-    v.type = LEPT_FALSE;
+    v.type = LEPT_TRUE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
     EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
@@ -66,7 +66,7 @@ static void test_parse_invalid_value() {
 
 static void test_parse_root_not_singular() {
     lept_value v;
-    v.type = LEPT_NULL;
+    v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
